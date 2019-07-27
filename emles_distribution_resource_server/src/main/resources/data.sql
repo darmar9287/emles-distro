5829e4adc4e2fb73be3c8621dbb4cb5ed37ece43
Dariusz <darglk@gmail.com>
2019-07-27 19:45:13 +0200

    changed project config for integration tests in resource server

--- emles_distribution_resource_server/src/main/resources/data.sql
+++ emles_distribution_resource_server/src/main/resources/data.sql
@@ -1,2 +1,16 @@
+INSERT INTO authority  VALUES(1,'ROLE_OAUTH_ADMIN');
+INSERT INTO authority VALUES(2,'ROLE_RESOURCE_ADMIN');
+INSERT INTO authority VALUES(3,'ROLE_PRODUCT_ADMIN');
+INSERT INTO app_user VALUES(1,TRUE,'oauth_admin','$2a$10$BurTWIy5NTF9GJJH4magz.9Bd4bBurWYG8tmXxeQh1vs7r/wnCFG2','0');
+INSERT INTO app_user VALUES(2,TRUE,'resource_admin','$2a$10$BurTWIy5NTF9GJJH4magz.9Bd4bBurWYG8tmXxeQh1vs7r/wnCFG2','0');
+INSERT INTO app_user  VALUES(3,TRUE,'product_admin','$2a$10$BurTWIy5NTF9GJJH4magz.9Bd4bBurWYG8tmXxeQh1vs7r/wnCFG2','0');
+INSERT INTO app_user_authorities VALUES (1,1);
+INSERT INTO app_user_authorities VALUES (2,2);
+INSERT INTO app_user_authorities VALUES (3,3);
+
+
+INSERT INTO oauth_client_details VALUES('curl_client','product_api', '$2a$10$BurTWIy5NTF9GJJH4magz.9Bd4bBurWYG8tmXxeQh1vs7r/wnCFG2', 'read,write', 'password', 'http://127.0.0.1', 'ROLE_PRODUCT_ADMIN', 7200, 0, NULL, 'true');
+
+
 INSERT INTO product (product_id, product_name,  product_price, product_quantity_left) VALUES (1, 'SOS', 9.99, 10);
 INSERT INTO product (product_id, product_name,  product_price, product_quantity_left) VALUES (2, 'SOS 1', 9.99, 10);
