d8d7e5d055dd50df5a98beb9c94bc193340ebae5
Dariusz <darglk@gmail.com>
2019-08-15 11:47:17 +0200

    added listing of authorities for oauth admin

---
+++ emles_oauth_server/src/main/java/com/emles/controller/AuthorityController.java
@@ -0,0 +1,25 @@
+package com.emles.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.emles.service.AuthorityService;
+
+@RestController
+@RequestMapping("/authority")
+public class AuthorityController {
+
+	@Autowired
+	private AuthorityService authorityService;
+	
+	@PreAuthorize("hasAuthority('ROLE_OAUTH_ADMIN')")
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ResponseEntity<?> showAuthorities() {
+		return ResponseEntity.ok().body(authorityService.listAuthorities());
+	}
+}
