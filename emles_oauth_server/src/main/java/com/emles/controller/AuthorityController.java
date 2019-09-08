dc51c4e405a65f6821cd72425babde489f960c0d
Dariusz <darglk@gmail.com>
2019-09-08 12:20:36 +0200

    added javadoc for rest of class

--- emles_oauth_server/src/main/java/com/emles/controller/AuthorityController.java
+++ emles_oauth_server/src/main/java/com/emles/controller/AuthorityController.java
@@ -9,13 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.emles.service.AuthorityService;
 
+/**
+ * Controller class for listing user authorities.
+ * @author Dariusz Kulig
+ *
+ */
 @RestController
 @RequestMapping("/authority")
 public class AuthorityController {
 
+	/**
+	 * authorityService - service for maintaining Authority instances.
+	 */
 	@Autowired
 	private AuthorityService authorityService;
 
+	/**
+	 * Endpoint where oauth admin can view authorities.
+	 * @return JSON object with list of authorities.
+	 */
 	@PreAuthorize("hasAuthority('ROLE_OAUTH_ADMIN')")
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public ResponseEntity<?> showAuthorities() {
--- emles_oauth_server/src/main/java/com/emles/controller/UsersController.java
+++ emles_oauth_server/src/main/java/com/emles/controller/UsersController.java
@@ -29,10 +29,21 @@ import com.emles.model.UserPasswords;
 import com.emles.model.projection.UserSimplified;
 import com.emles.utils.Utils;
 
+/**
+ * Controller class containing endpoints for signed and not signed in users.
+ * @author Dariusz Kulig
+ *
+ */
 @RestController
 @RequestMapping("/user")
 public class UsersController extends UserControllerBase {
 
+	/**
+	 * Endpoint used for revoking approval for signed in user.
+	 * @param approval - approval instance to be revoked.
+	 * @return JSON object containing message about successful approval revocation. When approval data will be
+	 * malformed, then 400 error will be sent.
+	 */
 	@PreAuthorize("hasAnyAuthority('ROLE_OAUTH_ADMIN', 'ROLE_PRODUCT_ADMIN', 'ROLE_USER')")
 	@RequestMapping(value = "/revoke_my_approval", method = RequestMethod.POST)
 	public ResponseEntity<?> revokeMyApproval(@RequestBody Approval approval) {
@@ -46,6 +57,10 @@ public class UsersController extends UserControllerBase {
 		return ResponseEntity.badRequest().build();
 	}
 
+	/**
+	 * Endpoint where signed in user can view their approvals.
+	 * @return JSON object containing list of approvals.
+	 */
 	@PreAuthorize("hasAnyAuthority('ROLE_OAUTH_ADMIN', 'ROLE_PRODUCT_ADMIN', 'ROLE_USER')")
 	@RequestMapping(value = "/my_approvals", method = RequestMethod.GET)
 	public ResponseEntity<?> myApprovals() {
@@ -55,6 +70,11 @@ public class UsersController extends UserControllerBase {
 		return ResponseEntity.ok().body(responseMap);
 	}
 
+	/**
+	 * Endpoint where signed in user can delete his account. After this action user account will be removed and all
+	 * tokens will be purged.
+	 * @return JSON object containing message about successful account removal.
+	 */
 	@PreAuthorize("hasAnyAuthority('ROLE_OAUTH_ADMIN', 'ROLE_PRODUCT_ADMIN', 'ROLE_USER')")
 	@RequestMapping(value = "/my_account/delete", method = RequestMethod.DELETE)
 	public ResponseEntity<?> deleteAccount() {
@@ -66,6 +86,10 @@ public class UsersController extends UserControllerBase {
 		return ResponseEntity.ok().body(responseMap);
 	}
 
+	/**
+	 * Endpoint where signed in user can view his account data.
+	 * @return JSON object conatining non-sensitive user data.
+	 */
 	@PreAuthorize("hasAnyAuthority('ROLE_OAUTH_ADMIN', 'ROLE_PRODUCT_ADMIN', 'ROLE_USER')")
 	@RequestMapping(value = "/my_account", method = RequestMethod.GET)
 	public ResponseEntity<?> showMyAccount() {
@@ -74,6 +98,13 @@ public class UsersController extends UserControllerBase {
 		return ResponseEntity.ok().body(user);
 	}
 
+	/**
+	 * Endpoint where user can activate his account with activation token.
+	 * @param id - user id which used to activate account.
+	 * @param token - token string value used to activate account.
+	 * @return - JSON object containing error validation result when user id or token is invalid. When all data provided
+	 * by user will be correct, then instead of error messages success message will be sent.
+	 */
 	@RequestMapping(value = "/validate_user_account", method = RequestMethod.POST)
 	public ResponseEntity<?> validateUser(@RequestParam("id") long id, @RequestParam("token") String token) {
 		Map<String, Object> responseMap = new HashMap<>();
@@ -87,6 +118,12 @@ public class UsersController extends UserControllerBase {
 		return ResponseEntity.ok().body(responseMap);
 	}
 
+	/**
+	 * Endpoint where signed in user can update his account.
+	 * @param userData - new user data.
+	 * @param errors - validation errors.
+	 * @return JSON object containing validation errors or account update success message.
+	 */
 	@PreAuthorize("hasAnyAuthority('ROLE_OAUTH_ADMIN', 'ROLE_PRODUCT_ADMIN', 'ROLE_USER')")
 	@RequestMapping(value = "/update_account", method = RequestMethod.PUT)
 	public ResponseEntity<?> updateAccountData(@Valid @RequestBody UserData userData, Errors errors) {
@@ -94,6 +131,15 @@ public class UsersController extends UserControllerBase {
 		return changeUserData(userData, errors, user);
 	}
 
+	/**
+	 * Endpoint where signed in user can change his password. After successful password change, new access token will be
+	 * generated.
+	 * @param request - http servlet request with access token stored in headers.
+	 * @param passwords - new user password.
+	 * @param errors - validation errors.
+	 * @return JSON object containing new access token and success message when all data will be correct. Otherwise
+	 * validation error messages will be sent.
+	 */
 	@PreAuthorize("hasAnyAuthority('ROLE_OAUTH_ADMIN', 'ROLE_PRODUCT_ADMIN', 'ROLE_USER')")
 	@RequestMapping(value = "/change_password", method = RequestMethod.POST)
 	public ResponseEntity<?> changePassword(HttpServletRequest request, @Valid @RequestBody UserPasswords passwords,
@@ -118,8 +164,14 @@ public class UsersController extends UserControllerBase {
 		return ResponseEntity.ok().body(responseMap);
 	}
 
+	/**
+	 * Endpoint where all users can request for password reset token when they forget their password.
+	 * @param email - email of user who's forgotten his password.
+	 * @return JSON object containing error message when email address will be invalid. When all data will be correct,
+	 * then successful message will be sent.
+	 */
 	@RequestMapping(value = "/forgot_password", method = RequestMethod.POST)
-	public ResponseEntity<?> resetPassword(HttpServletRequest request, @RequestBody String email) {
+	public ResponseEntity<?> resetPassword(@RequestBody String email) {
 		Map<String, Object> responseMap = new HashMap<>();
 		AppUser user = userService.findByEmail(email.replace("\"", ""));
 		if (user == null) {
@@ -133,6 +185,15 @@ public class UsersController extends UserControllerBase {
 		return ResponseEntity.ok().body(responseMap);
 	}
 
+	/**
+	 * Endpoint where user sets new password after sending correct reset token.
+	 * @param newPassword - new password to be set.
+	 * @param errors - validation errors.
+	 * @param id - user id who's requesting for a new password.
+	 * @param token - account activation token.
+	 * @return JSON object containing validation errors when token or user id or passwords will be invalid. If all data
+	 * will be correct, then successful message will be returned.
+	 */
 	@RequestMapping(value = "/change_forgotten_password", method = RequestMethod.POST)
 	public ResponseEntity<?> changeForgottenPassword(@Valid @RequestBody Passwords newPassword, Errors errors,
 			@RequestParam("id") long id, @RequestParam("token") String token) {
@@ -161,8 +222,14 @@ public class UsersController extends UserControllerBase {
 		return ResponseEntity.ok().body(responseMap);
 	}
 
+	/**
+	 * Endpoint where non signed in user can create new account.
+	 * @param user - new user instance.
+	 * @param errors - validation errors.
+	 * @return JSON object containing validation errors or successful message when all data wil be correct.
+	 */
 	@RequestMapping(value = "/sign_up", method = RequestMethod.POST)
-	public ResponseEntity<?> signUp(HttpServletRequest request, @Valid @RequestBody AppUser user, Errors errors) {
+	public ResponseEntity<?> signUp(@Valid @RequestBody AppUser user, Errors errors) {
 		return signUpNewUser(user, errors, false);
 	}
 }
