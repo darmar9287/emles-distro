422980aa0faf3b182e7db7013657c30430d391a1
Dariusz Kulig <darek@MacBook-Air-Dariusz.local>
2019-09-26 18:39:56 +0200

    finished AppUserRepository unit tests

--- emles_oauth_server/src/test/java/com/emles/repository/AppUserRepositoryTest.java
+++ emles_oauth_server/src/test/java/com/emles/repository/AppUserRepositoryTest.java
@@ -8,6 +8,7 @@ import java.time.Instant;
 import java.util.Arrays;
 import java.util.Date;
 
+import javax.persistence.PersistenceException;
 import javax.validation.ConstraintViolationException;
 
 import org.junit.Before;
@@ -32,21 +33,23 @@ public class AppUserRepositoryTest {
 
 	@Autowired
 	private AppUserRepository appUserRepository;
-	
+
 	@Autowired
 	private AuthorityRepository authorityRepository;
-	
+
 	@Autowired
 	private TestEntityManager entityManager;
-	
+
 	private AppUser user;
-	
+
+	private AppUser secondUser;
+
 	@Before
 	public void setUp() {
 		Authority userAuthority = new Authority();
 		userAuthority.setAuthority("ROLE_USER");
 		authorityRepository.save(userAuthority);
-		
+
 		String newUserPassword = "h4$h3dPa$$";
 		user = new AppUser();
 		user.setEmail("newuser@emles.com");
@@ -57,8 +60,18 @@ public class AppUserRepositoryTest {
 		user.setLastPasswordResetDate(Date.from(Instant.now()));
 		user.setAuthorities(Arrays.asList(userAuthority));
 		user.setEnabled(true);
+
+		secondUser = new AppUser();
+		secondUser.setEmail("new_second_user@emles.com");
+		secondUser.setName("new_second_user");
+		secondUser.setPassword(newUserPassword);
+		secondUser.setPasswordConfirmation(newUserPassword);
+		secondUser.setPhone("700600666");
+		secondUser.setLastPasswordResetDate(Date.from(Instant.now()));
+		secondUser.setAuthorities(Arrays.asList(userAuthority));
+		secondUser.setEnabled(true);
 	}
-	
+
 	@Test
 	public void testCreateUserSuccess() {
 		entityManager.persist(user);
@@ -71,10 +84,100 @@ public class AppUserRepositoryTest {
 		assertEquals(found.getPhone(), user.getPhone());
 		assertTrue(found.isEnabled());
 	}
-	
+
 	@Test(expected = ConstraintViolationException.class)
 	public void testCreateUserWithTooShortNameFails() {
 		user.setName("abc");
+		entityManager.persist(user);
+	}
+
+	@Test(expected = ConstraintViolationException.class)
+	public void testCreateUserWithTooLongNameFails() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 5; i++) {
+			sb.append("ABCDEFGHIJKLMNOPRSTQUWYXZ");
+		}
+		String tooLongName = sb.toString();
+		user.setName(tooLongName);
+		entityManager.persist(user);
+	}
+
+	@Test(expected = ConstraintViolationException.class)
+	public void testCreateUserWithInvalidNameCharactersFails() {
+		String invalidCharsName = "&*&@@#,.'\\";
+		user.setName(invalidCharsName);
+		entityManager.persist(user);
+	}
+
+	@Test(expected = PersistenceException.class)
+	public void testCreateUserWithExistingNameFails() {
+		entityManager.persist(user);
+		secondUser.setName(user.getName());
+		entityManager.persist(secondUser);
+	}
+
+	@Test(expected = ConstraintViolationException.class)
+	public void testCreateUserWithInvalidPasswordFails() {
+		String invalidPassword = "abcdefgh";
+		user.setPassword(invalidPassword);
+		entityManager.persist(user);
+	}
+
+	@Test(expected = ConstraintViolationException.class)
+	public void testCreateUserWithInvalidPasswordConfirmationFails() {
+		String invalidPassword = "abcdefgh";
+		user.setPasswordConfirmation(invalidPassword);
+		entityManager.persist(user);
+	}
+
+	@Test(expected = ConstraintViolationException.class)
+	public void testCreateUserWithTooShortPasswordFails() {
+		String invalidPassword = "abcd";
+		user.setPassword(invalidPassword);
+		entityManager.persist(user);
+	}
+
+	@Test(expected = ConstraintViolationException.class)
+	public void testCreateUserWithTooLongPasswordFails() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 5; i++) {
+			sb.append("ABCDEFGHIJKLMNOPRSTQUWYXZabcd123&^%$#_");
+		}
+		String invalidPassword = sb.toString();
+		user.setPassword(invalidPassword);
+		entityManager.persist(user);
+	}
+
+	@Test(expected = ConstraintViolationException.class)
+	public void testCreateUserWithInvalidEmailFails() {
+		String invalidEmail = "testdddd.com";
+		user.setEmail(invalidEmail);
+		entityManager.persist(user);
+	}
+
+	@Test(expected = PersistenceException.class)
+	public void testCreateUserWithExistingEmailFails() {
+		entityManager.persist(user);
+		secondUser.setEmail(user.getEmail());
+		entityManager.persist(secondUser);
+	}
+
+	@Test(expected = ConstraintViolationException.class)
+	public void testCreateUserWithInvalidPhoneFails() {
+		user.setPhone("123-fdfbd");
+		entityManager.persist(user);
+	}
+
+	@Test(expected = PersistenceException.class)
+	public void testCreateUserWithExistingPhoneFails() {
+		entityManager.persist(user);
+		secondUser.setPhone(user.getPhone());
+		entityManager.persist(secondUser);
+	}
+
+	@Test(expected = PersistenceException.class)
+	public void testCreateUserFailsWhenLastPasswordResetDateIsNull() {
+		user.setLastPasswordResetDate(null);
 		entityManager.persist(user);
 	}
 }
